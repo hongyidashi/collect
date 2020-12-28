40c9cfdece61bf168611d6c8b0462880500f4b76
hongyidashi <1031749665@qq.com>
2020-12-28 10:33:18 +0800

    补全websocket内容

--- code/websocket-demo/src/main/java/com/collect/websocket/config/WebSocketConfig.java
+++ code/websocket-demo/src/main/java/com/collect/websocket/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 /**
  * 描述: WebSocket 配置类
@@ -22,7 +23,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/mydlq").withSockJS();
+        registry.addEndpoint("/mydlq")
+                // 设置允许跨域，设置为"*"则为允许全部域名
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     /**
@@ -32,9 +36,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 设置一个或者多个代理前缀，在 Controller 类中的方法里面发生的消息，会首先转发到代理从而发送到对应广播或者队列中。
-        registry.enableSimpleBroker("/topic");
         // 配置客户端发送请求消息的一个或多个前缀，该前缀会筛选消息目标转发到 Controller 类中注解对应的方法里
         registry.setApplicationDestinationPrefixes("/app");
+        // 设置一个或者多个代理前缀，在 Controller 类中的方法里面发送的消息，会首先转发到代理从而发送到对应广播或者队列中。
+        registry.enableSimpleBroker("/topic");
+    }
+
+    /**
+     * 添加 WebSocket 用户上、下线监听器
+     */
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.addDecoratorFactory(new HttpWebSocketHandlerDecoratorFactory());
     }
 }
