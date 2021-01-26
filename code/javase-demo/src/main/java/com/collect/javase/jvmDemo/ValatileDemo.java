75b1551a0cf79eecfdfe80d6ac5ada1bb9eec35b
pengguangqin <1312455164@qq.com>
2021-01-26 22:46:23 +0800

    volatile：非原子性测试

--- code/javase-demo/src/main/java/com/collect/javase/jvmDemo/ValatileDemo.java
+++ code/javase-demo/src/main/java/com/collect/javase/jvmDemo/ValatileDemo.java
@@ -17,7 +17,34 @@ class A {
 public class ValatileDemo {
 
     public static void main(String[] args) {
+        testNoAtom();
+    }
 
+    /**
+     * 测试非原子性
+     *      val++ 其实分为了三步，一个是取val的值，然后val+1.然后赋值给val
+     *      如果线程数量比较多的时候，两个线程同时取到了val=0
+     *      两个线程几乎同时在自己的工作空间修改为1
+     *      这个时候再不保证原子性的情况下，就会出现数据的错误写问题相当于少写了一次
+     */
+    private static void testNoAtom() {
+        A a = new A();
+        for(int i = 0; i < 20; i++) {
+            //注意这里的线程数最好设置大点，然后里面++的次数也尽量放大
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    a.val++;
+                }
+            }, i+"").start();
+        }
+
+        //默认是有两个线程，一个是main，一个是gc线程，所以这里得是2
+        while(Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+
+        //最终结果小于2000
+        System.out.println(a.val);
     }
 
     /**
